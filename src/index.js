c78fecdf0ea06320a0121dee07627e01c108daf1
MecyDev <macmc@tutanota.com>
2021-02-03 21:20:30 +0100

    adaptation tablette

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ const close = document.getElementById("menu-close");
 
 open.addEventListener("click", function () {
   const menu = document.getElementById("menu");
-  menu.classList.replace("visible-none", "visible");
+  menu.style.display = "block";
 });
 
 close.addEventListener("click", function () {
   const menu = document.getElementById("menu");
-  menu.classList.replace("visible", "visible-none");
+  menu.style.display = "none";
+});
+
+window.addEventListener("resize", function () {
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    const menu = document.getElementById("menu");
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
 });
